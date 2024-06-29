721d6c62422bdd6a591e861388e029446791c1c6
shiho-k <shii0327@yahoo.co.jp>
2024-06-29 13:44:23 +0900

    Javaプログラミング入門学習終了

---
+++ src/UseVariable.java
@@ -0,0 +1,13 @@
+
+public class UseVariable {
+
+    public static void main(String[] args) {
+        int number2;
+        String str2;
+        number2=100;
+        str2="xyz";
+        System.out.print(number2);
+        System.out.print(str2);
+    }
+
+}
