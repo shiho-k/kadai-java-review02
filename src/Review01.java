e4f2e3988b722d331f5a13f17a6821de9408cc30
shiho-k <shii0327@yahoo.co.jp>
2024-07-01 21:27:51 +0900

    課題提出です。

--- src/Review01.java
+++ src/Review01.java
@@ -3,13 +3,14 @@ public class Review01 {
 
     public static void main(String[] args) {
        int price=1500;
-       int taxPrice=tax(price);
-       int zeikomiPrice=price+taxPrice;
-       System.out.println(price+"円の商品の税込価格は"+zeikomiPrice+"円" +"(消費税は"+taxPrice+"円）です。");
+       int zei=10;
+       int result;
+       result=tax(price,zei);
+       System.out.println(price+"円の商品の税込価格は"+(price+result)+"円" +"(消費税は"+result+"円）です。");
 }
-    public static int tax(int price) {
-        int taxPrice=price*10/100;
-        return taxPrice;
+    public static int tax(int price,int zei) {
+        int result=price*zei/100;
+        return result;
 }
 }
 
