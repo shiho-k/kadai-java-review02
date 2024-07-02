59869c2a49a0f5967f964372344b2d1f93e6e736
shiho-k <shii0327@yahoo.co.jp>
2024-07-02 17:32:19 +0900

    課題提出です。

--- src/Review02.java
+++ src/Review02.java
@@ -2,24 +2,22 @@
 public class Review02 {
 
     public static void main(String[] args) {
-        for(int i=1;i<=100;i++) {
+        for (int i = 1; i <= 100; i++) {
 
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
 
-
-         if(i%3==0&&i%5==0) {
-            System.out.println("FizzBuzz");
-
-        }else if (i%3==0) {
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
 
-        }else if(i%5==0) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-        }
+            }
 
-        else {
+            else {
                 System.out.println(i);
 
-        }
+            }
         }
     }
 }
